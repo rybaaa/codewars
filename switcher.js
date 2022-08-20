7d5a540a523f10680f24bb7a2268f2419699135d
rybaaa <bolshayaryba22@gmail.com>
2022-08-20 22:02:36 +0300

    switch letters in string

---
+++ switcher.js
@@ -0,0 +1,15 @@
+function switcheroo(x){
+    let newStr = '';
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] == 'a') {
+            newStr += 'b';
+        }
+        else if (x[i] == 'b') {
+            newStr += 'a';
+        } else {
+            newStr += x[i];
+        }
+    }
+    return newStr;
+}
+console.log(switcheroo('aabbcc'))   // 'bbaacc'
