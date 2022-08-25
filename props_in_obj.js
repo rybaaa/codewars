5a367f7bb1b8e452dbc37801902ea6705fb19deb
unknown <bolshayaryba22@gmail.com>
2022-08-26 00:31:14 +0300

    add a property with value in every object in array

---
+++ props_in_obj.js
@@ -0,0 +1,13 @@
+var questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+  }, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+  }];
+  for (let i =0; i < questions.length; i++) {
+    questions[i]['usersAnswer'] = null;
+    }
+  console.log(questions);    //adds usersAnswer: null in every object in array
