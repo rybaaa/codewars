4c21d7eff68966f1c646a6b1e4d913f6fcc7ba8f
rybaaa <bolshayaryba22@gmail.com>
2023-03-16 23:29:07 +0100

    matrix transpose

---
+++ matrix transpose.js
@@ -0,0 +1,33 @@
+function transpose(matrix) {
+    let new_arr = []
+    if(matrix.length===1){
+        for (let i = 0; i<matrix[0].length; i++){
+            new_arr.push([matrix[0][i]])
+        }
+    } else {
+        for (let i = 0; i < matrix[0].length; i++) {
+            let count = 0
+            new_arr[i] = []
+            while (count < matrix.length) {
+                new_arr[i].push(matrix[count][i])
+                count++
+            }
+        }
+    }
+    return new_arr
+}
+
+
+
+//Write a function that outputs the transpose of a matrix - a new matrix where the columns and rows of the original are swapped.
+//
+// For example, the transpose of:
+//
+// | 1 2 3 |
+// | 4 5 6 |
+// is
+//
+// | 1 4 |
+// | 2 5 |
+// | 3 6 |
+// The input to your function will be an array of matrix rows. You can assume that each row has the same length, and that the height and width of the matrix are both positive.
