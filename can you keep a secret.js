f0a1050e2a03d4f4c4d55e0d70e64abb0f2b4056
rybaaa <bolshayaryba22@gmail.com>
2023-03-16 23:24:50 +0100

    can you keep a secret

---
+++ can you keep a secret.js
@@ -0,0 +1,25 @@
+function createSecretHolder(secret) {
+    let result = secret
+    function getSecret (){
+        return result
+    }
+    function setSecret(s){
+        result = s
+    }
+    return {
+        result,
+        getSecret,
+        setSecret
+    }
+}
+
+//There's no such thing as private properties on a coffeescript object! But, maybe there are?
+//
+// Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods
+//
+// getSecret() which returns the secret
+// setSecret() which sets the secret
+// obj = createSecretHolder(5)
+// obj.getSecret() # returns 5
+// obj.setSecret(2)
+// obj.getSecret() # returns 2
